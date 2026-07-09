f2fe1fc4e52816aaa25af8f6123772788070c96d
GifftyCode <ulokangozi@yahoo.com>
2026-07-09 05:09:13 +0100

    Initial commit: My intro

---
+++ intro.js
@@ -0,0 +1,13 @@
+const name = "Ngozi";
+let age = 31;
+let favLang = "JavaScript";
+
+console.log(
+  "My name is " +
+    name +
+    "," +
+    "I'm " +
+    age +
+    " years old and my favorite programing language is " +
+    favLang,
+);
