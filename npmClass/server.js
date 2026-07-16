88d596604d2c886be7f96f8116c3c825f92dbcf3
GifftyCode <ulokangozi@yahoo.com>
2026-07-16 10:03:55 +0100

    middleware

--- npmClass/server.js
+++ npmClass/server.js
@@ -1,6 +1,20 @@
 const express = require("express");
-const PORT = 3000;
+
 const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  // logs every request
+  console.log(`${req.method} ${req.url} - ${new Date()}`);
+  next(); // Pass to next handler required
+});
+
+app.post("/echo", (req, res) => {
+  res.json({ echoed: req.body });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello from express!");
