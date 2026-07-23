e605d49883a0ffafa91894f31c667783e196cb69
GifftyCode <ulokangozi@yahoo.com>
2026-07-23 21:31:44 +0100

    todos

---
+++ week3-crud-api/app.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+const todos = [
+  { id: 1, task: "Learn Node.js", completed: false },
+  { id: 2, task: "Build CRUD API", completed: false },
+];
+
+app.get("/todos", (req, res) => {
+  res.status(200).json(todos); // Send array as JSON
+});
+
+const createTodos = (req, res) => {
+  const newTodo = { id: todos.length + 1, ...req.body }; // Auto ID
+  todos.push(newTodo);
+
+  res.status(201).json(newTodo); // Echo back
+};
+
+app.post("/todos", createTodos);
+
+// Patch Updte - Partial
+app.patch("/todos/:id", (req, res) => {
+  const todo = todos.find((t) => t.id === parseInt(req.params.id)); // Array.find()
+  if (!todo) return res.status(400).json({ message: "Todo not found" });
+
+  Object.assign(todo, req.body); // Merge. e.g {completed: true}
+
+  res.status(200).json(todo);
+});
+
+// Delete - Remove
+app.delete("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const initialLength = todos.length;
+  todos = todos.filter((t) => t.id !== id); // Array.filter() - non-destructive
+
+  if (todos.length === initialLength)
+    return res.status(404).json({ error: "Not found" });
+  res.status(204).send(); // Silent success
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
